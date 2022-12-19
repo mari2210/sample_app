2e0850cfd1adee5ec3ad93a14672dfe47e7ce0a8
EC2 Default User <ec2-user@ip-172-31-0-93.ec2.internal>
2022-12-19 13:00:19 +0000

    [Start]3-5

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   # get 'lists/show'
   # .../lists/1 や.../lists/3に該当する
   get 'lists/:id'=>'lists#show',as:'list'
-  get 'lists/edit'
+  # get 'lists/edit'
+  get 'lists/:id/edit'=>'lists#edit' ,as:'edit_list'
+  patch 'lists/:id'=>'lists#update',as:'update_list'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
