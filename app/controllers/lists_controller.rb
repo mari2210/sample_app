53b79dfc3d5137a6807459585e51ff1054b5c468
EC2 Default User <ec2-user@ip-172-31-0-93.ec2.internal>
2022-12-19 14:58:15 +0000

    [Finish]3-11

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,13 +6,17 @@ class ListsController < ApplicationController
   # 以下を追加
   def create
     # 1.&2.データを受け取り新規登録するためのインスタンス作成
-    list=List.new(list_params)
+    @list=List.new(list_params)
     # 3.データをデータベースに保存するためのsaveメそッど実行
-    list.save
+    if @list.save
+      redirect_to list_path(@list.id)
+    else
+      render :new
+    end
     # 4.トップ画面にリダイレクト
     # redirect_to '/top'
     # 詳細画面にリダイレクト
-    redirect_to list_path(list.id)
+    #redirect_to list_path(list.id)
   end
 
   def index
@@ -34,9 +38,15 @@ class ListsController < ApplicationController
 
   end
 
+  def destroy
+    list=List.find(params[:id])
+    list.destroy
+    redirect_to '/lists'
+  end
+
   private
   #ストロングパラメータ
   def list_params
-    params.require(:list).permit(:title,:body)
+    params.require(:list).permit(:title,:body,:image)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
-  get 'lists/new'
-  get 'top'=>'homes#top'
-  post 'lists'=>'lists#create'
-  # get 'lists/index'
-  get 'lists'=>'lists#index'
-  # get 'lists/show'
-  # .../lists/1 や.../lists/3に該当する
-  get 'lists/:id'=>'lists#show',as:'list'
-  # get 'lists/edit'
-  get 'lists/:id/edit'=>'lists#edit' ,as:'edit_list'
-  patch 'lists/:id'=>'lists#update',as:'update_list'
+  # get 'lists/new'
+   get 'top'=>'homes#top'
+   resources :lists
+  # post 'lists'=>'lists#create'
+  # # get 'lists/index'
+  # get 'lists'=>'lists#index'
+  # # get 'lists/show'
+  # # .../lists/1 や.../lists/3に該当する
+  # get 'lists/:id'=>'lists#show',as:'list'
+  # # get 'lists/edit'
+  # get 'lists/:id/edit'=>'lists#edit' ,as:'edit_list'
+  # patch 'lists/:id'=>'lists#update',as:'update_list'
+  # delete 'lists/:id'=>'lists#destroy',as:'destroy_list'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
